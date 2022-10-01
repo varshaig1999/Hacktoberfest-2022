d4245ce46f1036dae89b3fa22c42e467bfe9d5b3
Soham Mirikar <82722112+sohamm20@users.noreply.github.com>
2022-10-01 21:23:11 +0530

    Update mostsignificantbit.py

--- Algorithms/Significant Bit/mostsignificantbit.py
+++ Algorithms/Significant Bit/mostsignificantbit.py
@@ -1,13 +1,11 @@
 # Number
-n = 1
+n = 5
 
 # Function to calculate most significant bit
 def msb(n):
     if n == 0:
         return -1
     
-    ans = 0
-
     for i in range(32, -1, -1):
         if (1 << i) & n:
             return (i + 1)
