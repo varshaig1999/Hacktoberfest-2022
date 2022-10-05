84cf59cc5ab626c5e81f546dffea5912feeeb9fd
PHAHAM KHAN <72141945+Phaham@users.noreply.github.com>
2022-10-05 19:43:35 +0530

    Update median_of_two_sorted_arrays.cpp
    A very good problem based on binary search algorithm

--- Algorithms/Cpp/Binary search/median_of_two_sorted_arrays.cpp
+++ Algorithms/Cpp/Binary search/median_of_two_sorted_arrays.cpp
@@ -1,4 +1,10 @@
-/*
+//👉 PROBLEM ON BINARY SEARCH -
+
+// 🎯PROBLEM NAME - MEDIAN OF TWO SORTED ARRAYS
+
+
+// 📌 PROBLEM STATEMENT ->
+/* 
 Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
 
 The overall run time complexity should be O(log (m+n)).
