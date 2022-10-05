2fa733d47f59c831872a95536b7754436b59a018
PHAHAM KHAN <72141945+Phaham@users.noreply.github.com>
2022-10-05 13:06:25 +0530

    Create sudoku_solver.cpp

---
+++ Algorithms/Cpp/Backtracking/sudoku_solver.cpp
@@ -0,0 +1,63 @@
+/*
+Write a program to solve a Sudoku puzzle by filling the empty cells.
+
+A sudoku solution must satisfy all of the following rules:
+
+Each of the digits 1-9 must occur exactly once in each row.
+Each of the digits 1-9 must occur exactly once in each column.
+Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+The '.' character indicates empty cells.
+
+ 
+
+Example 1:
+
+
+Input: board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
+Output: [["5","3","4","6","7","8","9","1","2"],["6","7","2","1","9","5","3","4","8"],["1","9","8","3","4","2","5","6","7"],["8","5","9","7","6","1","4","2","3"],["4","2","6","8","5","3","7","9","1"],["7","1","3","9","2","4","8","5","6"],["9","6","1","5","3","7","2","8","4"],["2","8","7","4","1","9","6","3","5"],["3","4","5","2","8","6","1","7","9"]]
+Explanation: The input board is shown above and the only valid solution is shown below:
+
+
+ 
+
+Constraints:
+
+board.length == 9
+board[i].length == 9
+board[i][j] is a digit or '.'.
+It is guaranteed that the input board has only one solution.
+*/
+
+    void solveSudoku(vector<vector<char>>& board) {
+     
+         helper(board,0,0);
+        return ; 
+    }
+    
+    bool helper(vector<vector<char>>& board,int i,int j)
+    {   if(i==9){return true;}
+        if(j==9){ return helper(board,i+1,0);}
+        if(board[i][j]!='.'){return helper(board,i,j+1);}
+             
+     for(int number=1;number<=9;number++)
+        { if(can_place(board,i,j,'0'+ number))
+            {  board[i][j]='0'+ number;
+            
+              bool can_solve=helper(board,i,j+1);
+              if(can_solve==true){return true;}  
+            }              
+        }   
+        board[i][j]='.';
+        return false;
+    }   
+    
+    bool can_place(vector<vector<char>>& board,int i,int j,char val)
+    {
+       int row = i - i%3, column = j - j%3;
+    for(int x=0; x<9; x++) if(board[x][j] == val) return false;
+    for(int y=0; y<9; y++) if(board[i][y] == val) return false;
+    for(int x=0; x<3; x++)
+    for(int y=0; y<3; y++)
+        if(board[row+x][column+y] == val) return false;
+    return true;
+}
